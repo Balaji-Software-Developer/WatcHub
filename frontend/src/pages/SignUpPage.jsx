c873fa18961ff52234fdb1707b1e62a8ced2d06a
Balaji-Software-Developer <smartbalajisl75@gmail.com>
2025-03-12 16:01:40 +0530

    first project of My clone app using React

---
+++ frontend/src/pages/SignUpPage.jsx
@@ -0,0 +1,34 @@
+import { Link } from "react-router-dom";
+const SignUpPage = () => {
+    return(
+        <div className='h-screen w-full hero-bg'>
+        <header className='max-w-6xl mx-auto flex items-center justify-between p-4'>
+            <Link to={"/"}>
+            <img src='/netflix-logo.png' alt='logo' className='w-auto' style={{ width: '150px' }} 
+/>
+            </Link>
+        </header>
+    
+    <div className='flex justify-center items-center mt-20 mx-3'>
+				<div className='w-full max-w-md p-8 space-y-6 bg-black/60 rounded-lg shadow-md'>
+					<h1 className='text-center text-white text-2xl font-bold mb-4'>Sign Up</h1>
+
+					<form className='space-y-4'>
+						<div>
+							<label htmlFor='email' className='text-sm font-medium text-gray-300 block'>
+								Email
+							</label>
+							<input 
+                            type='email'
+								className='w-full px-3 py-2 mt-1 border border-gray-700 rounded-md bg-transparent text-white focus:outline-none focus:ring'
+								placeholder='you@example.com'
+								id='email'
+							/>
+						</div>
+                        </form>
+                        </div>
+                        </div>
+                        </div>
+    );
+};
+export default SignUpPage;
---
+++ frontend/src/pages/home/HomePage.jsx
@@ -0,0 +1,4 @@
+const HomePage = () => {
+    return <div className='hero-bg w-screen h-screen'>HomePage</div>;
+};
+export default HomePage;
